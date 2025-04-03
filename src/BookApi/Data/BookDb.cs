c5cc629631ec754a3a1c07fb84f4162b87e50037
Naomie-adj <107333@ecole-it.com>
2025-04-03 16:12:23 +0200

    formulaire

--- src/BookApi/Data/BookDb.cs
+++ src/BookApi/Data/BookDb.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using BookApi.Models;
 
 namespace BookApi.Data;
 
 public class BookDb : DbContext
 {
     public BookDb(DbContextOptions<BookDb> options) : base(options) { }
+
+    public DbSet<Book> Books => set<Book();
 
     // TODO: Ajouter le DbSet pour les livres
 }
